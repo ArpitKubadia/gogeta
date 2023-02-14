12099dd94fe21cc8c06836e9cd40e97a815b03b4
Arpit Kubadia <arpitkubadia23@gmail.com>
2023-02-14 00:05:44 -0500

    Added i/o functionality

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	var in io.Reader
+
+	// define command-line flags
+	filePtr := flag.String("file", "", "input file path")
+	urlPtr := flag.String("url", "", "input URL")
+	flag.Parse()
+
+	// check which flag is provided
+	switch {
+	case *filePtr != "":
+		// open input file
+		file, err := os.Open(*filePtr)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+
+		in = file
+	case *urlPtr != "":
+		// read from input URL
+		in = strings.NewReader(*urlPtr)
+	default:
+		// read from standard input
+		in = os.Stdin
+	}
+
+	// create scanner for input
+	scanner := bufio.NewScanner(in)
+
+	// scan input line by line
+	for scanner.Scan() {
+		// process each line
+		line := scanner.Text()
+		fmt.Println(line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
 }
